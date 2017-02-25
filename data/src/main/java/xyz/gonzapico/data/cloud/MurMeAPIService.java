8cc78d0df7233fb23dfbf61325c56b961f887605
Gonzalo Fernández <gonzalo.fernandez.pico@gmail.com>
2017-02-25 19:56:26 +0100

    Retrofit service

--- data/src/main/java/xyz/gonzapico/data/cloud/MurMeAPIService.java
+++ data/src/main/java/xyz/gonzapico/data/cloud/MurMeAPIService.java
@@ -1,10 +1,11 @@
 package xyz.gonzapico.data.cloud;
 
+import java.util.List;
 import retrofit2.Response;
-import retrofit2.http.GET;
 import retrofit2.http.Headers;
 import retrofit2.http.POST;
 import retrofit2.http.Path;
+import xyz.gonzapico.data.entity.UserEntity;
 
 /**
  * Created by gfernandez on 25/02/17.
@@ -13,8 +14,9 @@ import retrofit2.http.Path;
 public interface MurMeAPIService {
 
   @Headers({
-      "User-Agent : "
-  }) @POST("/{config}/") rx.Observable<Response<EntityConfiguration>> config(
-      @Path(value = "config", encoded = true) String section);
-
+      "language:es_ES", "Content-Type:application/x-www-form-urlencoded"
+  }) @POST("/{config}/") rx.Observable<Response<List<UserEntity>>> users(
+      @Path(value = "keyAccess", encoded = true) String keyAccess,
+      @Path(value = "pushToken", encoded = true) String pushToken,
+      @Path(value = "idUser", encoded = true) String idUser);
 }
