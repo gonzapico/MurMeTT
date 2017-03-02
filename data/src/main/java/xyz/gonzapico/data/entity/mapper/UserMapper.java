60e06eb506cee04fb4d081d39df2ed5411c34e27
Gonzalo Fernández <gonzalo.fernandez.pico@gmail.com>
2017-03-02 10:08:27 +0100

    Added a new method to transform data

--- data/src/main/java/xyz/gonzapico/data/entity/mapper/UserMapper.java
+++ data/src/main/java/xyz/gonzapico/data/entity/mapper/UserMapper.java
@@ -1,6 +1,9 @@
 package xyz.gonzapico.data.entity.mapper;
 
+import java.util.ArrayList;
 import java.util.List;
+import javax.inject.Inject;
+import javax.inject.Singleton;
 import retrofit2.Response;
 import xyz.gonzapico.data.entity.UserEntity;
 import xyz.gonzapico.domain.model.UserModelDomain;
@@ -9,8 +12,22 @@ import xyz.gonzapico.domain.model.UserModelDomain;
  * Created by gfernandez on 25/02/17.
  */
 
-public class UserMapper {
+@Singleton public class UserMapper {
+  @Inject public UserMapper() {
+
+  }
+
   public List<UserModelDomain> transformToListOfUsers(Response<List<UserEntity>> listResponse) {
-    return null;
+    List<UserModelDomain> listOfUsers = null;
+    if (listResponse.isSuccessful()){
+      listOfUsers = new ArrayList<>();
+      for (UserEntity userEntity : listResponse.body()){
+        UserModelDomain userModelDomain = new UserModelDomain();
+        userModelDomain.setName(userEntity.getName());
+
+        listOfUsers.add(userModelDomain);
+      }
+    }
+    return listOfUsers;
   }
 }
