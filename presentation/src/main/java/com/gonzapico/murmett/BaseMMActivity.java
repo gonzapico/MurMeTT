bd89555079fd01a873fd537aa8b0a362d50499a6
Gonzalo Fernández <gonzalo.fernandez.pico@gmail.com>
2017-03-02 10:10:48 +0100

    Added DI

--- presentation/src/main/java/com/gonzapico/murmett/BaseMMActivity.java
+++ presentation/src/main/java/com/gonzapico/murmett/BaseMMActivity.java
@@ -1,28 +1,85 @@
 package com.gonzapico.murmett;
 
+import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
-import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import butterknife.BindView;
-import butterknife.ButterKnife;
+import com.gonzapico.murmett.di.HasComponent;
+import com.gonzapico.murmett.di.components.ApplicationComponent;
+import com.gonzapico.murmett.di.components.DaggerUserComponent;
+import com.gonzapico.murmett.di.components.UserComponent;
+import com.gonzapico.murmett.di.modules.ActivityModule;
 import xyz.gonzapico.murmett.R;
 
 /**
  * Created by gfernandez on 26/02/17.
  */
 
-public abstract class BaseMMActivity extends AppCompatActivity {
+public abstract class BaseMMActivity extends AppCompatActivity
+    implements HasComponent<UserComponent> {
+  //@Inject Navigator navigator;
+  UserComponent mUserComponent;
+  Toolbar toolbar;
 
-  @BindView(R.id.toolbar) Toolbar mToolbar;
-
-  @Override public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
-    super.onCreate(savedInstanceState, persistentState);
-
+  @Override protected void onCreate(@Nullable Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
     setContentView(getLayoutResource());
 
-    ButterKnife.bind(this);
+    toolbar = (Toolbar) findViewById(R.id.toolbar);
+    if (toolbar != null) {
+      setSupportActionBar(toolbar);
+
+      getSupportActionBar().setTitle("");
+    }
+
+    this.initializeInjector();
   }
 
-  public abstract int getLayoutResource();
+  /***
+   * Method to init the DI and has all the classes availables
+   */
+  private void initializeInjector() {
+    this.mUserComponent = DaggerUserComponent.builder()
+        .applicationComponent(getApplicationComponent())
+        .activityModule(getActivityModule())
+        .build();
+  }
+
+  protected abstract int getLayoutResource();
+
+  /**
+   * Adds a {@link Fragment} to this activity's layout.
+   *
+   * @param containerViewId The container view to where add the fragment.
+   * @param fragment The fragment to be added.
+   */
+  protected void addFragment(int containerViewId, Fragment fragment) {
+    FragmentTransaction fragmentTransaction = this.getFragmentManager().beginTransaction();
+    fragmentTransaction.add(containerViewId, fragment);
+    fragmentTransaction.commit();
+  }
+
+  /**
+   * Get the Main Application component for dependency injection.
+   *
+   * @return {@link ApplicationComponent}
+   */
+  protected ApplicationComponent getApplicationComponent() {
+    return ((BaseMMApplication) getApplication()).getApplicationComponent();
+  }
+
+  /**
+   * Get an Activity module for dependency injection.
+   *
+   * @return {@link ActivityModule}
+   */
+  protected ActivityModule getActivityModule() {
+    return new ActivityModule(this);
+  }
+
+  @Override public UserComponent getComponent() {
+    return mUserComponent;
+  }
 }
