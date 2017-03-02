ffa17fd7170fee215e52076186180e3d41933fdd
Gonzalo Fernández <gonzalo.fernandez.pico@gmail.com>
2017-03-02 10:04:38 +0100

    Moved logic from activity to fragment

--- presentation/src/main/java/com/gonzapico/murmett/showUsers/UserModel.java
+++ presentation/src/main/java/com/gonzapico/murmett/showUsers/UserModel.java
@@ -5,4 +5,32 @@ package com.gonzapico.murmett.showUsers;
  */
 
 public class UserModel {
+
+  private String name = "";
+  private String urlPresentation = "";
+  private String pathLastAudio = "";
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getUrlPresentation() {
+    return urlPresentation;
+  }
+
+  public void setUrlPresentation(String urlPresentation) {
+    this.urlPresentation = urlPresentation;
+  }
+
+  public String getPathLastAudio() {
+    return pathLastAudio;
+  }
+
+  public void setPathLastAudio(String pathLastAudio) {
+    this.pathLastAudio = pathLastAudio;
+  }
 }
