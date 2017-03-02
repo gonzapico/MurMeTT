cbee17133fc0c8440b7b89e52567394bc6e56345
Gonzalo Fernández <gonzalo.fernandez.pico@gmail.com>
2017-03-02 10:08:49 +0100

    Removed lambdas

--- data/src/main/java/xyz/gonzapico/data/repository/UsersRepository.java
+++ data/src/main/java/xyz/gonzapico/data/repository/UsersRepository.java
@@ -3,7 +3,10 @@ package xyz.gonzapico.data.repository;
 import java.util.List;
 import javax.inject.Inject;
 import javax.inject.Singleton;
+import retrofit2.Response;
 import rx.Observable;
+import rx.functions.Func1;
+import xyz.gonzapico.data.entity.UserEntity;
 import xyz.gonzapico.data.entity.mapper.UserMapper;
 import xyz.gonzapico.data.repository.datasource.UserDataStore;
 import xyz.gonzapico.data.repository.datasource.UserDataStoreFactory;
@@ -25,8 +28,14 @@ import xyz.gonzapico.domain.repository.UsersDomainRepository;
     this.userMapper = userMapper;
   }
 
-  @Override public Observable<List<UserModelDomain>> getUsers(RequestAPIModelDomain requestAPIModelDomain) {
+  @Override
+  public Observable<List<UserModelDomain>> getUsers(RequestAPIModelDomain requestAPIModelDomain) {
     final UserDataStore userDataStore = this.userDataStoreFactory.createCloudDataStore();
-    return userDataStore.users(requestAPIModelDomain).map(this.userMapper::transformToListOfUsers);
+    return userDataStore.users(requestAPIModelDomain)
+        .map(new Func1<Response<List<UserEntity>>, List<UserModelDomain>>() {
+          @Override public List<UserModelDomain> call(Response<List<UserEntity>> listResponse) {
+            return userMapper.transformToListOfUsers(listResponse);
+          }
+        });
   }
 }
