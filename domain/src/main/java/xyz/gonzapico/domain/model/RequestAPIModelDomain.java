c4e09420d97c27ac23dbe0c409c82cb31b77e2a0
Gonzalo Fernández <gonzalo.fernandez.pico@gmail.com>
2017-03-02 10:05:07 +0100

    Wrap in a class the parameters of the request to the API

---
+++ domain/src/main/java/xyz/gonzapico/domain/model/RequestAPIModelDomain.java
@@ -0,0 +1,54 @@
+package xyz.gonzapico.domain.model;
+
+/**
+ * Created by gfernandez on 1/03/17.
+ */
+
+public class RequestAPIModelDomain {
+
+  private String keyAccess;
+  private String language;
+  private String pushToken;
+  private String contentType;
+  private String idUser;
+
+  public String getKeyAccess() {
+    return keyAccess;
+  }
+
+  public void setKeyAccess(String keyAccess) {
+    this.keyAccess = keyAccess;
+  }
+
+  public String getLanguage() {
+    return language;
+  }
+
+  public void setLanguage(String language) {
+    this.language = language;
+  }
+
+  public String getPushToken() {
+    return pushToken;
+  }
+
+  public void setPushToken(String pushToken) {
+    this.pushToken = pushToken;
+  }
+
+  public String getContentType() {
+    return contentType;
+  }
+
+  public void setContentType(String contentType) {
+    this.contentType = contentType;
+  }
+
+  public String getIdUser() {
+    return idUser;
+  }
+
+  public void setIdUser(String idUser) {
+    this.idUser = idUser;
+  }
+}
