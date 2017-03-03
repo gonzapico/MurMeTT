ba02c9764a49836a9e2a2edb6cef35a171480742
Gonzalo Fernández <gonzalo.fernandez.pico@gmail.com>
2017-03-03 17:01:02 +0100

    Added parameters as static strings

--- data/src/main/java/xyz/gonzapico/data/Config.java
+++ data/src/main/java/xyz/gonzapico/data/Config.java
@@ -6,4 +6,9 @@ package xyz.gonzapico.data;
 
 public class Config {
   public static final String API_URL_BASE = "http://murmedesa.quadram.mobi:8024";
+  public static final String LANGUAGE = "es_ES";
+  public static final String KEY_ACCESS = "M_rM3Q";
+  public static final String ID_USER = "3f797096-8ddd-4aaa-8135-66f652c8a8b2";
+  public static final String PUSH_TOKEN =
+      "e2FDw4A_OyQ:APA91bERDeEBdys28VminIdDslu7as1lOHS_8kSoQgtXSyCxu6AGWMj_cTFuTu74O2e7pE6Ds_Jb7rlaBo3z0v1Eyj_d2N2WjZTZ8eSkCHKvYuIYEVDubL7Yc86otoy0TpV5YYThQ8v2";
 }
